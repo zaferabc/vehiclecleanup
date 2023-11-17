1df8169e287ac1522e8fd4b7ad579a760d57c819
Zafer Abacı <148140510+zaferabc@users.noreply.github.com>
2023-11-17 17:25:34 +0300

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SK_VehicleCleanup")]
-[assembly: AssemblyDescription("Unturned LDM (RocketMod) plugin to remove unused vehicles.")]
+[assembly: AssemblyDescription("Unturned plugin to remove unused vehicles.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SK_VehicleCleanup")]
